fd5a794852e37dc3ba82b53b44dca24b564233ec
Chris Pynegar <chris@chrispynegar.co.uk>
2015-03-26 18:32:16 +0000

    Added file system constants

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,3 +84,15 @@ define('EXIT_USER_INPUT', 7); // invalid user input
 define('EXIT_DATABASE', 8); // database error
 define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+|--------------------------------------------------------------------------
+| File System
+|--------------------------------------------------------------------------
+|
+| Helpers with file paths and writing files
+|
+*/
+define('DS', DIRECTORY_SEPARATOR);
+define('NL', "\n");
+define('TB', "\t");
