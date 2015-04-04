16a89e250302cf3f62c28eae0b81b5c48f0cf4ad
Chris Pynegar <chris@chrispynegar.co.uk>
2015-04-04 22:14:04 +0100

    Added url_helper override

---
+++ application/helpers/MY_url_helper.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ( ! function_exists('admin'))
+{
+    /**
+     * Check if we are viewing the admin area
+     *
+     * @return bool
+     */
+    function admin()
+    {
+        return get_instance()->uri->segment(1) === 'admin';
+    }
+}
+
+if ( ! function_exists('admin_url'))
+{
+    /**
+     * site_url function prefixed with admin/
+     *
+     * @param string $url
+     * @return string
+     */
+    function admin_url($url)
+    {
+        return site_url('admin/'.$url);
+    }
+}
+
+if ( ! function_exists('refresh'))
+{
+    /**
+     * Reload current page
+     *
+     * @return void
+     */
+    function refresh()
+    {
+        redirect(current_url(), 'refresh');
+    }
+}
